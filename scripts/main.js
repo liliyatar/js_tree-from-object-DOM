b034e5b7358fb9b0f590ac85712fc17ea0705d7e
Liliya Taran <liliyataran2017@gmail.com>
2019-11-28 08:29:23 +0200

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const food = {
+  'Meat': {
+    'Pork': {},
+    'Beef': {},
+  },
+
+  'Fruit': {
+    'Red': {
+      'Cherry': {},
+      'Strawberry': {},
+    },
+    'Yellow': {
+      'Banana': {},
+      'Pineapple': {},
+    },
+
+  },
+};
+
+const tree = document.querySelector('#tree');
+
+function createTree(element, data) {
+  element.innerHTML = createList(data);
+}
+
+function createList(data) {
+  let list = '';
+
+  for (const key in data) {
+    list += `<li>${key}${createList(data[key])}</li>`;
+  }
+
+  return `<ul>${list}</ul>`;
+}
+
+createTree(tree, food);
